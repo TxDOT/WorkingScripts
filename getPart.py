4cacbbe777cfc3d9ba08bb2b595c1292e5ff33fb
UnchartedDrake <davidhickman13@gmail.com>
2013-11-21 09:04:55 -0600

    Variou

--- getPart.py
+++ getPart.py
@@ -4,7 +4,7 @@ def get_Part():
 
     import arcpy
 
-    table = # Path to a shapefile
+    table = "C:\\_GRID_SHAREPOINT_TEST\\4TextData\\02_20_2013\\BaseMapLayers\\ADJ_STATE_POLY.shp" # Path to a shapefile
 
     desc = arcpy.Describe(table)
     shapefieldname = desc.ShapeFieldName
@@ -14,16 +14,16 @@ def get_Part():
     for row in rows:
         feat = row.getValue(shapefieldname)
 
-        print "Feature" + row.getValue("RTE_ID")
+        print "Feature" + str(row.getValue("OBJECTID"))
         partnum = 0
 
         print "Part %i:" % partnum
 
-        for pnt in feat.getPart(partnum):
-            if pnt:
-                print pnt.X, pnt.Y, pnt.M
-            else:
-                pass
+        #for pnt in feat.getPart(partnum):
+            #if pnt:
+                ##print pnt.X, pnt.Y, pnt.M
+            #else:
+                #pass
 
         partnum += 1
 
